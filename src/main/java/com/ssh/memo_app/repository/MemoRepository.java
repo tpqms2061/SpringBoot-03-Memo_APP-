ace2c214c47185a511b2bc0af4d30643525ba345
tpqms2061 <tpqms2061@naver.com>
2025-07-04 16:00:44 +0900

    Second

--- src/main/java/com/ssh/memo_app/repository/MemoRepository.java
+++ src/main/java/com/ssh/memo_app/repository/MemoRepository.java
@@ -2,6 +2,8 @@ package com.ssh.memo_app.repository;
 
 
 import com.ssh.memo_app.model.Memo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,9 @@ import java.util.List;
 @Repository
 public class MemoRepository {
     private final JdbcTemplate jdbcTemplate; //JDBC를 사용하기 위한 만들어져있는 템플릿
+
+    @Autowired
+    private Environment env; //환경 변수 확인할수 있는 객체
 
     public MemoRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -22,9 +27,11 @@ public class MemoRepository {
                     resultSet.getString("title"),
                     resultSet.getString("content")
             ); //RowMapper<Memo>  ResultSet의 각 행을 Memo 객체로 변환 resultset : 행의 데이터 / rowNum : 행의 번호
+
     //resultSet.getInt("id")	DB 컬럼 "id" 값을 정수로 가져옴
 
-    public List<Memo> findAll() { //전체 조회
+    public List<Memo> findAll() {
+        System.out.println("APP_NAME:" + env.getProperty("APP_NAME"));//전체 조회
         return jdbcTemplate.query(
                 "Select * FROM memo ORDER BY id DESC",
                 memoRowMapper
@@ -39,15 +46,11 @@ public class MemoRepository {
         );
     }
 
-
-
-
-
     public void save(String title, String content) {
-         jdbcTemplate.update(
-                 "INSERT INTO memo (title, content) VALUES (?, ?)",
-                 title, content         ); //()안에 내용은 query문 - db문구
-         // update 는 db정보를 바꾸겠다는 의미  //query 가 조회하는것
+        jdbcTemplate.update(
+                "INSERT INTO memo (title, content) VALUES (?, ?)",
+                title, content); //()안에 내용은 query문 - db문구
+        // update 는 db정보를 바꾸겠다는 의미  //query 가 조회하는것
 
     }// 메모를 하나 생성하는 기능 , id값은 자동으로 채워지고 ,제목이랑 내용만 적어주면됨
 
@@ -57,7 +60,6 @@ public class MemoRepository {
                 title, content, id
         );
     }
-
 
 
     public void delete(int id) {
